708e2780a663fb7650d731dc9320d21014c00370
scottbamfordJ <75759730+scottbamfordJ@users.noreply.github.com>
2021-11-17 22:36:56 -0500

    Made Outline for Scatter Plot

--- Project/main.js
+++ Project/main.js
@@ -108,9 +108,6 @@ usSates.on("click", (ev, d) => {
     bargraph_init();
    
 })
-/* 
-Making the BarGraph based upon Clicked information
-*/
 
 
 
@@ -130,124 +127,7 @@ svg.on("mousemove", (ev) => {
 })
  draw(); // calls the draw function
 }
-// function bargraph_1_init() {
 
-//     const storage_Graph2 = state.click_info
-//     yScale = d3.scaleBand ()
-//     //Order the data
-//     //Make bar pop out 
-//         .domain(state.bargraph.Organizations)
-//         .range([ height_1 - margin_1, margin_1])
-//         .padding(0.2)
-//     xScale = d3.scaleLinear()
-//         // Using the 0, as the starting point, and having to duble subtract margins ( Look a tbargraph.append("g" style.) )
-//         .domain([0, d3.max(state.bargraph.Attacks_Done)])
-//         .range([ 0, width_1 - margin_1 - margin_1])  
-//     yAxis = d3.axisRight(yScale)
-//         .scale(yScale)
-//     xAxis = d3.axisBottom(xScale)
-//         .scale(xScale)
-//     const bargraph = d3.select("#bargraph")
-//         .append("svg")
-//         .attr("width", width_1)
-//         .attr("height", height_1) 
-//     bargraph_1 = bargraph.selectAll(".bar")
-//         .data(state.bargraph)
-//         .enter().append("rect")
-//         .join()
-//         .attr("class", "bar")
-//         // .on("mousemove", organization_name)
-//         .attr("y", d => {
-//           return yScale(d[0])
-//         } )
-//         // .attr("x",  d => yScale(d[1]))
-//         .attr("x",  margin_1)
-//         // .attr("width", d =>  width_1 - margin_1 - xScale(d[1]))
-//         .attr("width", d => xScale(d[1]))
-//         .attr("height", yScale.bandwidth)
-//         .attr("fill", "lightblue")
-
-//         /* on click function for the output */
-//         // State creation use state_click info - > refer to above. 
-//     bargraph.append('g')
-//         .call( xAxis )
-//         .attr('class', 'x-axis')
-//         .style("transform", `translate(${margin_1}px,${height_1 - margin_1}px)`)
-//         // .attr("dx", "2em")
-    
-//     bargraph.append('g')
-//         .call(yAxis)
-//         .attr('class', 'y-axis')
-//         .style("transform", `translate(${margin_1}px,0px)`)   
-//     xAxisGroup = svg.append("g")
-//         .attr("class", 'xAxis')
-//         .attr("transform", `translate(${0}, ${height - 20})`) // move to the bottom
-//         .call(xAxis)
-    
-//     yAxisGroup = svg.append("g")
-//         .attr("class", 'yAxis')
-//         .attr("transform", `translate(${20}, ${0})`) // align with left margin
-//         .call(yAxis)
-        
-//     draw_bargraph_1()
-
-// }
-// function draw_bargraph_1(){
-//     let prevXScale = xScale.copy();
-//     let prevYScale = yScale.copy();
-
-//     xScale = xScale.domain([0, d3.max(state.bargraph.Attacks_Done)])
-//     xAxisGroup 
-//         .transition()
-//         .duration(1000)
-//         .call(xAxis.scale(xScale))
-//     yScale = yScale.domain(state.bargraph.Organizations)
-//     yAxisGroup  
-//         .transition()
-//         .duration(1000)
-//         .call(yAxis.scale(yScale))
-//     const bars = bargraph.append("rect")
-//         .data(state.bargraph)
-//         .join(
-//             enter => enter.append("rect")
-//                 .attr("y", d => {
-//                     return yScale(d[0])
-//                 } )
-//                 .attr("x",  margin_1)
-//                 .attr("width", d => xScale(d[1]))
-//                 .attr("height", yScale.bandwidth)
-//                 .attr("fill", "lightblue")
-//                 .attr("class", "bar")
-//                 .call(sel => sel.transition().duration(1000)
-//                     .attr("y", d => {
-//                         return yScale(d[0])
-//                     } )
-//                     .attr("x",  margin_1)
-//                     .attr("width", d => xScale(d[1]))
-//                     .attr("height", yScale.bandwidth)
-//                 ),
-//             update => update.call(sel => sel.transition()
-//                 .duration(1000)
-//                 .attr("y", d => {
-//                     return yScale(d[0])
-//                 } )
-//                 .attr("x",  margin_1)
-//                 .attr("width", d => xScale(d[1]))
-//                 .attr("height", yScale.bandwidth)
-//             ),
-//             exit => exit.call(exit => exit.transition()
-//                         .duration(1000)
-//                         .attr("y", d => {
-//                             return yScale(d[0])
-//                         } )
-//                         .attr("x",  margin_1)
-//                         .attr("width", d => xScale(d[1]))
-//                         .attr("height", yScale.bandwidth)
-//                         .style("opacity", 0)
-//                         .remove()),
-            
-//             )
-// }
 
 
 /**
@@ -345,15 +225,8 @@ function draw_scatter() {
     console.log(test_storage)
     var parse = d3.timeParse("%Y")
     
-    xScale = d3.scaleLinear()
-    // Using the 0, as the starting point, and having to duble subtract margins ( Look a tbargraph.append("g" style.) )
-        .domain([0, d3.max(state.scatter_plot_kills.value)])
-        .range([ 0, width_1 - margin_1 - margin_1])  
-    console.log(xScale.domain())
-    yAxis = d3.axisRight(yScale)
-      .scale(yScale)
-    xAxis = d3.axisBottom(xScale)
-      .scale(xScale)
+
+
     // console.log(storage_scatter)
     // console.log(state.scatter_plot_kills )
     // console.log(state.scatter_plot_wounded)
@@ -361,35 +234,99 @@ function draw_scatter() {
         .domain([parse(1970), parse(2018)])
         .range([margin_1, width_1 - margin_1])
     yScale_scatter = d3.scaleLinear()
-        .domain(d3.extent(storage_scatter, d=> d.nkills))
+        .domain(d3.extent(state.scatter_plot_kills, d=> d.value))
         .range([height_1 - margin_1, margin_1])
-    console.log(yScale_scatter)
+
     const xAxis_scatter = d3.axisBottom(xScale_scatter)
+        .ticks(30)
 
     const yAxis_scatter = d3.axisLeft(yScale_scatter)
 
-    scatter = d3.select("#scatter-plot")
+    const bargraph = d3.select("#scatter-plot")
         .append("svg")
         .attr("width", width_1)
-        .attr("height", height_1)
-    xAxisGroup = scatter.append("g")
-        .attr('class', 'xAxis')
-        .call(xAxis_scatter)
-    xAxisGroup.append("text")
-        .attr("class", "xLabel")
-        .text("Year")
-    yAxisGroup = scatter.append("g")
-        .attr("class", "yAxis")
-        .call(yAxis_scatter)
-    //ALL INFO IS IN state.Bargraph_2info
-    const lineGen = d3.line()
-        .x(d=> xScale_scatter(state.scatter_plot_kills.value))
-        .y(d => yScale_scatter(state.scatter_plot_kills.key))
-    scatter.selectAll(".trend")
+        .attr("height", height_1) 
+
+    bargraph_1 = bargraph.selectAll(".bar")
         .data(state.scatter_plot_kills)
-        .join("path")
-        .attr("class", "trend")
-        .attr("stroke", "black")
-        .attr("fill", "none")
-        .attr("d", d => lineGen(d))
+        .join(
+            enter => enter.append("rect")
+            .attr("class", "bar")
+            .attr("y", yScale_scatter())
+            .attr("x",  margin_1)
+            .attr("width", d => xScale_scatter(d.keys))
+            .attr("height", d => yScale_scatter(d.values))
+            .attr("fill", "lightblue")
+        )
+    bargraph.append('g')
+        .call( xAxis_scatter )
+        .attr('class', 'x-axis')
+        .style("transform", `translate(${0}px,${width_1 - margin_1}px)`)
+    
+    bargraph.append('g')
+        .call(yAxis_scatter)
+        .attr('class', 'y-axis')
+        .style("transform", `translate(${margin_1}px,0px)`)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // scatter = d3.select("#scatter-plot")
+    //     .append("svg")
+    //     .attr("width", width_1)
+    //     .attr("height", height_1)
+    // xAxisGroup = scatter.append("g")
+    //     .attr('class', 'xAxis')
+    //     .call(xAxis_scatter)
+    // xAxisGroup.append("text")
+    //     .attr("class", "xLabel")
+    //     .text("Year")
+    // yAxisGroup = scatter.append("g")
+    //     .attr("class", "yAxis")
+    //     .call(yAxis_scatter)
+    // //ALL INFO IS IN state.Bargraph_2info
+    // const lineGen = d3.line()
+    //     .x(d=> xScale_scatter(state.scatter_plot_kills.value))
+    //     .y(d => yScale_scatter(state.scatter_plot_kills.key))
+    // scatter.selectAll(".trend")
+    //     .data(state.scatter_plot_kills)
+    //     .join("path")
+    //     .attr("class", "trend")
+    //     .attr("stroke", "black")
+    //     .attr("fill", "none")
+    //     .attr("d", d => lineGen(d))
 }
